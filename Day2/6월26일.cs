8506dfc2da867b7cd63f98384b068f59638c4840
dldgusdn312 <dlgusdn312@gmail.com>
2024-06-26 17:11:52 +0900

    Update 6월26일.cs

--- Day2/6월26일.cs
+++ Day2/6월26일.cs
@@ -171,3 +171,22 @@ namespace FirstFor
     }
 }
 ```
+```
+namespace whileApp
+{
+internal class Program
+{
+static void Main(string[] args)
+{
+int i = 1;
+int sum = 0;
+
+        while (i <= 100)
+        {
+            sum += i++;
+        }
+        Console.WriteLine(sum);
+    }
+}
+}
+```
