25511793edcf5d59c3db5f79bf90057032971faa
dldgusdn312 <dlgusdn312@gmail.com>
2024-06-26 14:47:19 +0900

    Update 1.cs

--- Day2/1.cs
+++ Day2/1.cs
@@ -103,3 +103,25 @@ Console.WriteLine(Math.PI);
 double area = Math.PI * radius * radius;
 Console.WriteLine($"{area:F2}");
 ```
+```
+namespace quiz02
+{
+
+    internal class Program
+
+    {
+        static void Main(string[] args)
+
+        {
+
+            Console.Write("반지름을 입력하시오 : ");
+
+            double radius = double.Parse(Console.ReadLine());
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine($"반지름 넓이 : {area:F2}");
+
+        }
+     }
+    }
+```
