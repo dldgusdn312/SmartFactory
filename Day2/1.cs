eb76d2ddf5a61f703e7e67e61250209bdd0a17aa
dldgusdn312 <dlgusdn312@gmail.com>
2024-06-26 15:29:23 +0900

    Update 1.cs

--- Day2/1.cs
+++ Day2/1.cs
@@ -125,3 +125,29 @@ namespace quiz02
      }
     }
 ```
+```
+namespace ifapp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            if (name == "홍길동")
+            {
+                Console.WriteLine("1. 나는 {0} 입니다", name);
+
+            }
+            else if(name == "이순신")
+            {
+                Console.WriteLine("2. 나는 {0} 입니다", name);
+            }
+            else
+            {
+                Console.WriteLine("3. 나는 {0} 입니다", name);
+            }
+        }
+    }
+}
+```
