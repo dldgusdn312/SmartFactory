a6c3aa96d82408bc086b05a4d86689146a7455a6
dldgusdn312 <dlgusdn312@gmail.com>
2024-06-27 09:11:47 +0900

    Update 6월27일.cs

--- Day3/6월27일.cs
+++ Day3/6월27일.cs
@@ -3,114 +3,57 @@
 namespace ConsoleApp8
 
 {
-
     internal class Program
-
     {
-
         static void Main(string[] args)
-
         {   
-
             int sum1 = 0;
-
             int sum2 = 0;
-
             for (int i = 0; i <= 100; i++)
-
             {
-
                 if (i % 2 == 0)
-
                 {
-
                     sum1 = sum1 + i;
-
                 }
-
                 else
-
                 {
-
    sum2 = sum2 + i;
-
                 }          
-
             }
-
             Console.WriteLine(sum1);
-
             Console.WriteLine(sum2);
-
         }
-
     }
-
 }
-
-
-
 
 
 2. 
-
 namespace ConsoleApp8
-
 {
-
     internal class Program
-
     {
-
         static void Main(string[] args)
-
         {
-
             int i = 1;
-
             int sum1 = 0;
-
             int sum2 = 0;
-
-         
-
             while(i<=100)
-
             {
-
                 if (i % 2 == 0)
-
                 {
-
                     sum1 = sum1 + i;
-
                 }
-
                 else
-
                 {
-
                     sum2 = sum2 + i;
-
                 }
-
                 i++;
-
             }
-
-
-
             Console.WriteLine(sum2);
-
             Console.WriteLine(sum1);
-
         }
-
     }
-
 }
-
-                 
-
+          
 }
 ```
