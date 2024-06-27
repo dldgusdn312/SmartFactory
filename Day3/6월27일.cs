92255d182422d7b624c448643c4213a7f2236744
dldgusdn312 <dlgusdn312@gmail.com>
2024-06-27 11:24:52 +0900

    Update 6월27일.cs

--- Day3/6월27일.cs
+++ Day3/6월27일.cs
@@ -175,4 +175,34 @@ namespace ExamApp01
     }
 }
 ```
+```
+<switch case문>
+namespace ExamApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("입력을 해주세요. (1.사과 2.배 3.오렌지) ... : ");
+            int number = Int32.Parse(Console.ReadLine());
+            
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("사과 입니다.");
+                    break;
+                    case 2:
+                    Console.WriteLine("배 입니다.");
+                    break;
+                    case 3:
+                    Console.WriteLine("오렌지 입니다.");
+                    break;
+                default:
+                    Console.WriteLine("메뉴가 없습니다.");
+                    break;
+            }
 
+        }
+    }
+}
+```
