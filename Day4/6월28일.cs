77ccf20356d3205409b6164022d0110834736b57
dldgusdn312 <dlgusdn312@gmail.com>
2024-06-28 15:23:55 +0900

    Update 6월28일.cs

--- Day4/6월28일.cs
+++ Day4/6월28일.cs
@@ -270,3 +270,29 @@ namespace Arayy03
     }
 }
 ```
+```
+<static 활용>
+namespace Quiz05
+{
+    internal class Program
+    {
+        static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        static void Main()
+        {
+            int v1 = 100;
+            int v2 = 200;
+            int plus = Plus(v1, v2);
+            int minus = Minus(v1, v2);
+            Console.WriteLine($"{plus} {minus}");
+        }
+    }
+}
+```
