ab4a80dc1f3b1c168a030a95a206326fe4c58f4d
dldgusdn312 <dlgusdn312@gmail.com>
2024-06-28 15:29:31 +0900

    Update 6월28일.cs

--- Day4/6월28일.cs
+++ Day4/6월28일.cs
@@ -296,3 +296,30 @@ namespace Quiz05
     }
 }
 ```
+```
+<Calculator 사용>
+namespace calcualtorApp
+{
+    class Calculator
+    {
+        public int Multiple(int a, int b)
+        {
+            return a * b;
+        }
+        public double Divide(int a, int b)
+        {
+            return (double)a / b;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            Console.WriteLine(cal.Multiple(5, 6));
+            Console.WriteLine(cal.Divide(100,5));
+        }
+    }
+}
+```
