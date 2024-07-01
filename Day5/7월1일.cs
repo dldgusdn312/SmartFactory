48519ccbde6a90f0fe57ed576b25b8e212eaff11
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-01 17:12:35 +0900

    Update 7월1일.cs

--- Day5/7월1일.cs
+++ Day5/7월1일.cs
@@ -282,3 +282,27 @@ namespace ConsoleApp6
     }
 }
 ```
+```
+<SwapByValue>
+namespace swapbyvalue
+{
+    internal class Program
+    {
+        static void Swap(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+
+            Console.WriteLine($"{a} {b}");
+        }
+        static void Main(string[] args)
+        {
+            int x = 3, y = 4;
+            Console.WriteLine($"{x} {y}");
+            Swap(x, y);
+            Console.WriteLine($"{x} {y}");
+        }
+    }
+}
+```
