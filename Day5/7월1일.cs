9159f4a550328526bb7379742e1b5c5ce5fc7118
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-01 09:29:30 +0900

    Update 7월1일.cs

--- Day5/7월1일.cs
+++ Day5/7월1일.cs
@@ -1,1 +1,22 @@
+```
+<변수교환>
+namespace VariableChange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
 
+            Console.WriteLine($"{a}_{b}");
+            //변수교환
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine($"{a} {b}");
+
+        }
+    }
+}
+```
