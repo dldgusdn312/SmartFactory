511341a8706d07ebbe105aeb5bc42f4f4bfdef64
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-02 09:20:50 +0900

    Update 7월2일.cs

--- Day6/7월2일.cs
+++ Day6/7월2일.cs
@@ -1,1 +1,19 @@
-
+```
+<do~while문 구구단 3단>
+namespace dowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine($"3 * {i} = {3*i} ");
+                i++;
+            } while (i<10);
+            }
+        }
+    }
+```
+```
