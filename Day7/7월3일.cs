f86f4fb2ebd07313d25e2cc2fccab4ce224f8873
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-03 09:51:00 +0900

    Update 7월3일.cs

--- Day7/7월3일.cs
+++ Day7/7월3일.cs
@@ -1,1 +1,47 @@
+```
+<오버로딩>
+namespace methodapp01
+{
+    class MyClass
+    {
+        public void Print()
+        {
+            Console.WriteLine("MyClass hello");
+        }
+        public void Print(string s)
+        {
+            Console.WriteLine(s);
+        }
+        public void Print(string s, double speed)
+        {
+            Console.WriteLine($"{s} , speed : {speed}");
+        }
+        public void Print(double speed, string s)
+        {
+            Console.WriteLine($"speed : {speed}, {s}");
+        }
+    } // end of MyClass
+    internal class Program
+    {
+        static void Print()
+        {
+            Console.WriteLine("hello");
+        }
+        public static void Print(string s)
+        {
+            Console.WriteLine(s);
+        }
+        static void Main(string[] args)
+        {
+            Print();
+            Print("안녕하세요");
 
+            MyClass mc = new MyClass();
+            mc.Print();
+            mc.Print("반갑습니다.");
+            mc.Print("수고하세요", 100);
+            mc.Print(100, "하이요");
+        }
+    }
+}
+```
