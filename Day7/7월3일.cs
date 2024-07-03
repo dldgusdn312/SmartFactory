7fea045401d4246d05f2257168bcbc1b28e8178d
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-03 10:20:01 +0900

    Update 7월3일.cs

--- Day7/7월3일.cs
+++ Day7/7월3일.cs
@@ -45,3 +45,52 @@ namespace methodapp01
     }
 }
 ```
+```
+<오버로딩 2>
+    using System.Data;
+
+namespace methodapp02
+{
+    class Bank
+    {
+        //1.멤버 변수
+        private int money;
+        //2.생성자
+        public Bank()
+        {
+            this.money = 10000;
+        }
+        //3.멤버 메소드
+        //4.예금하다
+        public void Deposit()
+        {
+            Console.WriteLine($"{money} 금액을 예금하다.");
+        }
+        public void Deposit(int money)
+        {
+            Console.WriteLine($"{money} 금액을 예금하다.");
+        }
+        //인출하다
+        public void WithDraw()
+        {
+            Console.WriteLine($"{money} 금액을 인출하다.");
+        }
+        //이체하다
+        public void Transfer()
+        {
+            Console.WriteLine($"{money} 금액을 이체하다.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank kb = new Bank();
+            kb.Deposit();
+            kb.Deposit(1000000);
+            kb.Transfer();
+            kb.WithDraw();
+        }
+    }
+}
+```
