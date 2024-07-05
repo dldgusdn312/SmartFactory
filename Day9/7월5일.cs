4d5dbb2a6dd4421d25a5aaa1a1bd5de102baed69
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-05 11:14:01 +0900

    Update 7월5일.cs

--- Day9/7월5일.cs
+++ Day9/7월5일.cs
@@ -76,3 +76,73 @@ namespace PrimeNumber
 }
 ```
 ```
+<quiz01>
+
+using System.Security.Cryptography;
+
+//1단계
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Random random = new Random();
+            int[] number = new int[5];
+            Console.Write("로또번호 : ");
+            for (int i = 0; i < 5; i++)
+            {
+                number[i] = random.Next(1, 46);
+                Console.Write($"{number[i]} ");
+            }
+         
+            Console.WriteLine($"보너스번호 : {random.Next(1, 46)}");
+        }
+    }
+}
+//2단계
+
+using System.Globalization;
+using System.Security.Cryptography;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[7];
+            Random random = new Random();
+
+            for (int i = 0; i < 7; i++)
+            {
+                numbers[i] = random.Next(1, 46);
+                //전수조사
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            int bonusNumber = numbers[6];
+            int[] lottoNumbers = new int[6];
+            Array.Copy(numbers, 0, lottoNumbers, 0, 6);
+
+            Array.Sort(lottoNumbers);
+            foreach (int i in lottoNumbers)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"보너스 번호 {bonusNumber}");
+
+        }
+    }
+}
+```
+```
