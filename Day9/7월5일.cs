d364a25c912363ed34425911415b917543f23d01
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-05 09:37:29 +0900

    Update 7월5일.cs

--- Day9/7월5일.cs
+++ Day9/7월5일.cs
@@ -1,1 +1,34 @@
+```
+<primeNumber>
 
+using System.Security.Cryptography;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        cnt++;
+                        break;
+                    }
+                    if (cnt == 0)
+                    {
+                        Console.WriteLine(i);
+
+                        cnt = 0;
+                    }
+                }
+            }
+        }
+    }
+}
+```
+```
