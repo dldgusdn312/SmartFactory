fa0699b1fc8c022decd777507e3412d03d4a98ba
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-05 14:34:39 +0900

    Update 7월5일.cs

--- Day9/7월5일.cs
+++ Day9/7월5일.cs
@@ -178,3 +178,33 @@ namespace LottoApp
 }
 ```
 ```
+<버블정렬>
+namespace bubblesort01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 4, 5, 7, 3, 2, 1, 9, 8 };
+            int temp;
+            for (int i = 8 - 1; i >= 0; i--)
+            {
+                for (int k = 0; k < i ; k++)
+                {
+                    if (list[k] > list[k + 1])
+                    {
+                        temp = list[k];
+                        list[k] = list[k + 1];
+                        list[k + 1] = temp;
+                    }
+                }
+            }
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+```
+```
