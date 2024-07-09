56c748a4043ec75947d99499f6cf12a7f74192fa
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-09 09:35:42 +0900

    Update 7월9일.cs

--- day11/7월9일.cs
+++ day11/7월9일.cs
@@ -19,3 +19,33 @@ namespace callbyvalue01
 }
 ```
 ```
+namespace MethodTest02
+{
+    internal class Program
+    {
+        //static void GetNumbers(out int x, out int y){
+            
+        //    x = 0;
+        //    y = 0;
+        //    }
+        static void GetValue(out int x)
+        {
+            x = 1;
+        }
+        static void Main(string[] args)
+        {
+            //    int a = int.Parse(Console.ReadLine());
+            //    int b = int.Parse(Console.ReadLine());
+            //    GetNumbers(out a, out b);
+
+            //Console.WriteLine($"a에 저장된 값은 {a}입니다.");
+            //Console.WriteLine($"a에 저장된 값은 {b}입니다.");
+            int a = 100;
+            GetValue(out a);
+            Console.WriteLine($"a에 저장된 값은 {a}");
+
+        }
+   }
+}
+```
+```
