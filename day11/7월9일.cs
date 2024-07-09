bfbb909add1a26d6845b293afd8711a855a46dd7
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-09 10:10:56 +0900

    Update 7월9일.cs

--- day11/7월9일.cs
+++ day11/7월9일.cs
@@ -68,3 +68,25 @@ namespace Recursive01
 }
 ```
 ```
+<팩토리얼 함수>
+namespace Facotrial01
+{
+    internal class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return n * Factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine(Factorial(a));
+
+        }
+    }
+}
+```
+```
