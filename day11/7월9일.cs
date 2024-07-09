3ba499d13579c60a7a73aa521d1db0833b2d0365
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-09 13:31:34 +0900

    Update 7월9일.cs

--- day11/7월9일.cs
+++ day11/7월9일.cs
@@ -166,3 +166,26 @@ namespace FactoriaRecursive
 }
 ```
 ```
+using System.ComponentModel.Design;
+
+namespace FactoriaRecursive
+{
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+       int n = 16;
+            int[] arr = new int[n + 1];
+            arr[0] = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                arr[i] = i * arr[i - 1];
+            }
+            foreach (int i in arr)
+                Console.WriteLine(i + "");
+        }
+    }
+}
+```
+```
