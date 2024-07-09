e3b4fa9bdc70992f8457bcd31c45c67d454e0227
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-09 10:27:21 +0900

    Update 7월9일.cs

--- day11/7월9일.cs
+++ day11/7월9일.cs
@@ -90,3 +90,30 @@ namespace Facotrial01
 }
 ```
 ```
+<팩토리얼 함수 교수님 정답>
+namespace Facotrial01
+{
+    internal class Program
+    {
+        static Dictionary<int, long> arr = new Dictionary<int, long>();
+                
+        static long Factorial(int n)
+        {
+            if (arr.ContainsKey(n))
+            {
+                return arr[n];
+            }
+            arr[n] = n * Factorial(n - 1);
+            return arr[n];
+        }
+        static void Main(string[] args)
+        {
+            int n = 7;
+            arr[0] = 1;
+            arr[1] = 1;
+            Console.WriteLine(Factorial(n));
+        }
+    }
+}
+```
+```
