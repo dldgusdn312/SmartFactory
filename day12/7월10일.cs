e692bf7f76f600b26d3b95118483a1953edeebc7
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-10 10:08:05 +0900

    Update 7월10일.cs

--- day12/7월10일.cs
+++ day12/7월10일.cs
@@ -1,1 +1,25 @@
+```
+namespace ArrayTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[7];//선언과 동시 초기화
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = 13 * (i + 1);
+                Console.WriteLine(numbers[i]);
+            }
+            string[] names = new string[3];
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                names[j] = Console.ReadLine();
+            }
+                Console.WriteLine(numbers[j]);
+            }
+        }
+    }
+```
+```
