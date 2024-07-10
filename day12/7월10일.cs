35495f21074f969c8e335f191162904690a6af23
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-10 10:27:45 +0900

    Update 7월10일.cs

--- day12/7월10일.cs
+++ day12/7월10일.cs
@@ -23,3 +23,53 @@ namespace ArrayTest
     }
 ```
 ```
+namespace ConsoleApp14
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "아무개";
+        public string Major { get; set; } = "공통학부";
+    }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Student[] students = new Student[3];
+                Student s1 = new Student();
+                Student s2 = new Student();
+                Student s3 = new Student();
+
+                students[0] = s1;
+                students[1] = s2;
+                students[2] = s3;
+
+                s1.Id = 1;
+                s1.Name = "홍길동";
+                s1.Major = "컴퓨터공학과";
+
+                s2.Id = 2;
+                s2.Name = "이순신";
+                s2.Major = "기계설계공학과";
+
+                s3.Id = 3;
+                s3.Name = "이현우";
+                s3.Major = "정보통신공학과";
+
+                foreach (Student s in students)
+                {
+                    Console.WriteLine(s.Id);
+                    Console.WriteLine(s.Name);
+                    Console.WriteLine(s.Major);
+                }
+                for (int i = 0; i < students.Length; i++)
+                {
+                    Console.WriteLine(students[i].Id);
+                    Console.WriteLine(students[i].Name);
+                    Console.WriteLine(students[i].Major);
+                }
+            }
+        }
+    }
+```
+```
