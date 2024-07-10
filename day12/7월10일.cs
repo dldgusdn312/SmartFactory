cad828c9860ed24f38c90cb38e1110fffafdfd01
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-10 11:42:08 +0900

    Update 7월10일.cs

--- day12/7월10일.cs
+++ day12/7월10일.cs
@@ -103,3 +103,30 @@ namespace ParamsTest
 }
 ```
 ```
+<Indexer>
+namespace indexerTest
+{
+    class IdxDemo
+    {
+        private int[] num = new int[5];
+
+        public int this[int x]{
+            set { num[x] = value; }
+            get { return num[x]; }
+            }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IdxDemo test = new IdxDemo();
+            for (int i = 0; i < 5; i++)
+            {
+                test[i] = i;
+                Console.WriteLine(test[i]);
+            }
+        }
+    }
+}
+```
+```
