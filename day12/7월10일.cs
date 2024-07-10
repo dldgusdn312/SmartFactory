c0663596dc378f96d531b091a9282208d533124a
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-10 11:33:06 +0900

    Update 7월10일.cs

--- day12/7월10일.cs
+++ day12/7월10일.cs
@@ -73,3 +73,33 @@ namespace ConsoleApp14
     }
 ```
 ```
+namespace ParamsTest
+{
+    internal class Program
+    {
+        static void TestMethod(double[] arr)
+        {
+
+        }
+        static int TotalSum(params int[] myArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                sum += myArray[i];
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            double[] arr2 = {1,2,3};
+            TestMethod(arr2);
+            //TestMethod(1,2,3,4,5);
+            //TotalSum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+            Console.WriteLine(TotalSum(1,2,3,4,5,6,7,8,9,10));
+            Console.WriteLine(TotalSum(1,3,5,7,9));
+        }
+    }
+}
+```
+```
