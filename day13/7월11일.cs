47ca4f1893d586ebb95e5da990e53849c7940f2e
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-11 13:47:12 +0900

    Update 7월11일.cs

--- day13/7월11일.cs
+++ day13/7월11일.cs
@@ -271,3 +271,42 @@ namespace interFace03
 }
 ```
 ```
+namespace Delegate02
+{
+
+    internal class Program
+    {
+        public delegate int Compute(int a, int b);
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 5;
+            Compute compute = Plus;
+            Console.WriteLine($"덧셈을 하게 됩니다. : {compute(x, y)}");
+            compute = Minus;
+            Console.WriteLine($"덧셈을 하게 됩니다. : {compute(x, y)}");
+            compute = Multiple;
+            Console.WriteLine($"곱셈을 하게 됩니다. : {compute(x, y)}");
+            compute = Divide;
+            Console.WriteLine($"나눗셈을 하게 됩니다. : {compute(x, y)}");
+        }
+        public static int Plus(int a, int b)
+        {
+        return a + b; 
+        }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiple(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
+```
+```
