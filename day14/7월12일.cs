7490f75a3cfc84564cb919e462c743b749f0ca25
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-12 10:05:40 +0900

    Update 7월12일.cs

--- day14/7월12일.cs
+++ day14/7월12일.cs
@@ -1,1 +1,37 @@
+```
+namespace OOPdelegateApp
+{
+    class Station
+    {
+        public string Policestation()
+        {
+            return "경찰서에 신고하다";
+        }
+        public string Firestaion()
+        {
+            return "소방서에 신고하다";
+        }
+        public string Tax()
+        {
+            return "국세청에 신고하다";
+        }
+    }
 
+    internal class Program
+    {
+        delegate string Report();
+        static void Main(string[] args)
+
+        {
+            Station st = new Station();
+            Report report = st.Policestation;
+            Console.WriteLine(report());
+            report = st.Firestaion;
+            Console.WriteLine(report());
+            report = st.Tax;
+            Console.WriteLine(report());
+        }
+    }
+}
+```
+```
