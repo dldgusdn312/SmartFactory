0342413b4e85ebc6fc25cfd699b55f540bfa6152
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-12 14:37:57 +0900

    Update 7월12일.cs

--- day14/7월12일.cs
+++ day14/7월12일.cs
@@ -168,3 +168,88 @@ namespace ListTestApp02
 }
 ```
 ```
+<quiz3>
+namespace ConsoleApp17
+{
+    class Person
+    {
+        //private int id;
+        //private string name;
+        //private string hp;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hp { get; set; }
+
+        public Person(int id, string name, string hp)
+        {
+            Id = id;
+            Name = name;
+            Hp = hp;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> addressbook = new List<Person>();
+
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("1. 데이터 삽입");
+                Console.WriteLine("2. 데이터 삭제");
+                Console.WriteLine("3. 데이터 수정");
+                Console.WriteLine("4. 데이터 검색");
+                Console.WriteLine("5. 프로그램 종료");
+                Console.WriteLine();
+                Console.Write("메뉴 : ");
+                choice = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("삽입");
+                        Console.Write("ID 번호를 입력하세요. :");
+                        int id = int.Parse(Console.ReadLine());
+                        Console.Write("이름을 입력하세요. :");
+                        string name = Console.ReadLine();
+                        Console.Write("전화번호를 입력하세요. :");
+                        string hp = Console.ReadLine();
+                        Person person = new Person(id, name, hp);
+                        addressbook.Add(person);
+                        
+
+                        break;
+                    case 2:
+                        Console.WriteLine("삭제");
+                        break;
+                    case 3:
+                        Console.WriteLine("수정");
+                        break;
+                    case 4:
+                        Console.WriteLine("검색");
+                        foreach(Person p in addressbook)
+                        {
+                            Console.WriteLine($"ID : {p.Id}");
+                            Console.WriteLine($"NAME : {p.Name}");
+                            Console.WriteLine($"HP : {p.Hp}");
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("종료");
+                        break;
+                        default:
+                        Console.WriteLine("다시 시작해 주세요.");
+                        break;
+                }
+                Console.WriteLine();
+
+            } while (choice != 5) ;
+        }
+    }
+}
+```
+```
