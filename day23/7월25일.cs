578e6f91783514f4be0a4eba962f7cc8898b8a84
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-25 10:30:49 +0900

    Update 7월25일.cs

--- day23/7월25일.cs
+++ day23/7월25일.cs
@@ -69,3 +69,25 @@ namespace ConsoleApp18
 }
 ```
 ```
+<stack>
+namespace StackTest01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            while (stack.Count > 0)
+            {
+
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
+```
+```
