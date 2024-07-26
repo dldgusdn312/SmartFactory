ab050ff9e966d98d98bc29b41303a6cd8f07d201
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-26 10:45:19 +0900

    Update 7월26일.cs

--- day24/7월26일.cs
+++ day24/7월26일.cs
@@ -20,3 +20,46 @@ namespace ListExam01
 }
 ```
 ```
+<퀴즈 1 교집합 구하기>
+  static void Main(string[] args)
+
+        {
+
+            List<int> list1 = new List<int>() {1, 2, 2, 3, 4 };
+
+            List<int> list2 = new List<int>() {2, 3, 5, 6 };
+
+            List<int> list3 = new List<int>();
+
+            for (int i = 0; i < list1.Count; i++)
+
+            {
+
+                for (int j = 0; j < list2.Count; j++)
+
+                {
+
+                    if (list1[i] == list2[j] && !(list3.Contains(list1[i])))
+
+                    {
+
+                        list3.Add(list2[j]);
+
+                        Console.WriteLine(list2[j]);
+
+                        break;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+```
+```
