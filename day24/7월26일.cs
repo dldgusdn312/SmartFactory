e8eb82b389ee38112d6b1d337ba1229160b771ab
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-26 10:09:40 +0900

    Update 7월26일.cs

--- day24/7월26일.cs
+++ day24/7월26일.cs
@@ -1,1 +1,22 @@
-
+```
+<list>
+namespace ListExam01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr =new List<int>();
+            for(int i =0; i<100; i++)
+            {
+                arr.Add(i+1);
+            }
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+```
+```
