fd60180344308f7fc31405cf369337d41ed06a94
dldgusdn312 <dlgusdn312@gmail.com>
2024-07-30 10:10:43 +0900

    Update 7월30일.cs

--- day26/7월30일.cs
+++ day26/7월30일.cs
@@ -1,1 +1,37 @@
+```
 
+namespace BoxingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 number1 = 0;
+            int number2 = 100;
+
+            Double number3 = 3.14;
+            double number4 = 31.4159;
+
+            number1 = number2;
+            Console.WriteLine(number1);
+
+            number2 = number1;
+            Console.WriteLine(number2);
+
+            number3 = number4;
+            number4 = number3;
+
+            char ch1 = 'A';
+            char ch2 = 'B';
+            ch1 = ch2;
+            ch2 = ch1;
+
+            float f1 = 3.14F;
+            Double f2 = f1;
+            f1 = (float)f2;
+            
+        }
+    }
+}
+```
+```
