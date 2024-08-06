483f24e85b5706d4d00df05e88d9c3b3bb40052c
dldgusdn312 <dlgusdn312@gmail.com>
2024-08-06 10:12:46 +0900

    Update 8월6일.cs

--- day28/8월6일.cs
+++ day28/8월6일.cs
@@ -1,1 +1,24 @@
-
+```
+<연습문제>
+namespace WinFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Sajin = 1;
+        int Sajin_Max = 3;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(System.Environment.CurrentDirectory + "/ㅎㅎ/" + Sajin + ".jpg");
+            Sajin++;
+            if (Sajin > Sajin_Max)
+                Sajin = 1;
+        }
+    }
+}
+```
+```
