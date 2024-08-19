f921f0f838531de760c1970a5bd4ff07ca11f6a9
dldgusdn312 <dlgusdn312@gmail.com>
2024-08-19 14:41:58 +0900

    Update 8월19일.cs

--- day36/8월19일.cs
+++ day36/8월19일.cs
@@ -124,3 +124,34 @@ namespace ThreadQuiz
 }
 ```
 ```
+<WorkThread>
+namespace WorkThread01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Main Thread
+            int threadCount = 5;
+
+            Thread[] threads = new Thread[threadCount];
+            for (int i = 0; i < threadCount; i++)
+            {
+                int threadIndex = i;
+                threads[i] = new Thread(() => DoWork(threadIndex));
+                threads[i].Start();
+            }
+            foreach (Thread thread in threads)
+                thread.Join();
+            Console.WriteLine("모든 작업이 완료되었습니다.");
+        }
+        static void DoWork(int index)
+        {
+            Console.WriteLine($"스레드 {index} 시작 : 작업을 수행 중...");
+            Thread.Sleep(1000);
+            Console.WriteLine($"스레드 {index} 완료 : 작업이 끝났습니다.");
+        }
+    }
+}
+```
+```
